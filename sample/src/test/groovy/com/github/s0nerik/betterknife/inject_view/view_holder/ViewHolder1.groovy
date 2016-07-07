140c432cb075c53e913a4a8a3c03c669e8aac7b6
sonerik <thesonerik@gmail.com>
2016-07-07 06:22:34 +0300

    Big cleanup.

--- sample/src/test/groovy/com/github/s0nerik/betterknife/inject_view/view_holder/ViewHolder1.groovy
+++
@@ -1,7 +0,0 @@
-package com.github.s0nerik.betterknife.inject_view.view_holder
-
-import groovy.transform.CompileStatic
-
-@CompileStatic
-class ViewHolder1 {
-}
--- sample/src/test/groovy/com/github/s0nerik/betterknife/util/SampleSpecification.groovy
+++
@@ -1,8 +0,0 @@
-package com.github.s0nerik.betterknife.util
-import org.junit.runner.RunWith
-import pl.polidea.robospock.RoboSpecification
-
-@RunWith(SampleSputnik)
-abstract class SampleSpecification extends RoboSpecification {
-
-}
